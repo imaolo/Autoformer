6a78916cf773f2b2019e559c615d0cd0a520a955
mad360 <matt@dldipaolo.com>
2024-07-29 23:40:58 -0700

    copy time feature code from informer2020

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -253,15 +253,8 @@ class Dataset_Custom(Dataset):
 
         df_stamp = df_raw[['date']][border1:border2]
         df_stamp['date'] = pd.to_datetime(df_stamp.date)
-        if self.timeenc == 0:
-            df_stamp['month'] = df_stamp.date.apply(lambda row: row.month, 1)
-            df_stamp['day'] = df_stamp.date.apply(lambda row: row.day, 1)
-            df_stamp['weekday'] = df_stamp.date.apply(lambda row: row.weekday(), 1)
-            df_stamp['hour'] = df_stamp.date.apply(lambda row: row.hour, 1)
-            data_stamp = df_stamp.drop(columns=['date'], axis=1).values
-        elif self.timeenc == 1:
-            data_stamp = time_features(pd.to_datetime(df_stamp['date'].values), freq=self.freq)
-            data_stamp = data_stamp.transpose(1, 0)
+        data_stamp = time_features(df_stamp, timeenc=self.timeenc, freq=self.freq)
+
 
         self.data_x = data[border1:border2]
         self.data_y = data[border1:border2]
